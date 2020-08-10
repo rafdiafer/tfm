394cfad449e007e5b7eb2d98eaf9b830fa386598
root <root@10.0.3.15>
2020-08-10 23:04:28 +0200

    Saving from centos

--- sendLogs.sh
+++ sendLogs.sh
@@ -1,2 +1,4 @@
-fw log $FWDIR/log/fw.log > $FWDIR/log/exportedLogs/$(date +%Y_%m_%d).log
-scp Rafa@192.168.102.2 -i 
+FECHA=$(date +%Y_%m_%d)
+fw log $FWDIR/log/fw.log >> $FWDIR/log/exportLogs/${FECHA}.log
+scp $FWDIR/log/exportLogs/${FECHA}.log dit@192.168.102.3:/home/dit/tfm/logs/${FECHA}.log
+rm $FWDIR/log/exportLogs/${FECHA}.log
